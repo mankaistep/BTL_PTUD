74b75e40185edac05bf5266d758766bd33a27637
mankaistep <hhoang21vn@gmail.com>
2021-05-22 20:36:39 +0700

    Add Student Object

---
+++ BTL_PTUD/Source/Objects/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_PTUD.Source.Objects {
+
+    class Student {
+
+        public readonly string ID;
+        public readonly string Name;
+        public readonly DateTime Date;
+        public readonly string Department;
+
+        public Student(string id, string name, DateTime date, string department) {
+            this.ID = id;
+            this.Name = name;
+            this.Date = date;
+            this.Department = department;
+        }
+
+
+    }
+
+}
